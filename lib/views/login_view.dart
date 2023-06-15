33c23eeaac30e8144f88ccf54d65fd4befda59ad
xxANONYMxx <samuel.lange@drei.at>
2023-06-15 09:27:51 +0200

    changed login_view.dart to a stateless widget

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -3,48 +3,32 @@ import 'package:toast/toast.dart';
 import 'package:visions/services/auth/auth_service.dart';
 import 'package:visions/components/square_tile.dart';
 
-
-class LoginView extends StatefulWidget {
+class LoginView extends StatelessWidget {
   const LoginView({Key? key}) : super(key: key);
-
-  @override
-  State<LoginView> createState() => _LoginViewState();
-}
-
-class _LoginViewState extends State<LoginView> {
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
     ToastContext().init(context);
     return Scaffold(
-        appBar: AppBar(
-            title: const Text("Sign in with Google")
+      appBar: AppBar(
+        title: const Text("Sign in with Google"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 200),
+            const SizedBox(
+              child: Text(
+                "Login with your Google account!",
+              ),
+            ),
+            SquareTile(
+              onTap: () => AuthService().signInWithGoogle(),
+              imagePath: 'lib/images/google.png',
+            ),
+          ],
         ),
-        body: Center(
-          child: Column(
-            children: [
-              SizedBox(height: 200),
-                SizedBox(child: Text(
-                  "Login with your Google account!",
-                ),
-              ),
-              SquareTile(
-                onTap: () =>  AuthService().signInWithGoogle(),
-                imagePath: 'lib/images/google.png',
-              ),
-            ],
-          ),
-        )
+      ),
     );
   }
 }
